c931bf3fa39ec0063d92f8baf4f6272e4da372d0
hinata <hnoguchi132@outlook.jp>
2025-10-08 09:57:43 +0900

    [fix]RuboCopによる自動修正実行

--- db/migrate/20251007014559_devise_create_users.rb
+++ db/migrate/20251007014559_devise_create_users.rb
@@ -6,7 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[8.0]
       ## 必須
       t.string  :nickname, null: false
       t.integer :role,     null: false, default: 0
-      
+
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
