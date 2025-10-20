5866ebddc1878902884d7950ec69785898214cd3
hinata <hnoguchi132@outlook.jp>
2025-10-20 17:15:46 +0900

    [update]フェス出演アーティスト一覧ページ

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,10 +1,36 @@
 class ArtistsController < ApplicationController
   def index
-    base   = Artist.order(:name)
+    @festival = Festival.find_by(id: params[:festival_id])
+
+    base = 
+    if @festival
+      @festival_days = @festival.festival_days.order(:date)
+      @selected_festival_day = if params[:festival_day_id].present?
+        @festival_days.find_by(id: params[:festival_day_id])
+      end
+      @selected_festival_day ||= @festival_days.first
+
+      if @selected_festival_day
+        Artist
+          .joins(stage_performances: :festival_day)
+          .where(stage_performances: { festival_day_id: @selected_festival_day.id })
+          .distinct
+          .includes(stage_performances: :festival_day)
+      else
+        Artist.none
+      end
+    else
+      @festival_days = []
+      @selected_festival_day = nil
+      Artist.all
+    end
+
+    base = base.order(:name)
+
     @q     = base.ransack(params[:q])
     result = @q.result(distinct: true)
 
-    @pagy, @artists = pagy(result, params: { q: params[:q] })
+    @pagy, @artists = pagy(result, params: request.query_parameters)
   end
 
   def show
