499f15911d4dc317d4f7d1c433f0dfa3b8e95652
hinata <hnoguchi132@outlook.jp>
2025-11-16 10:17:36 +0900

    [refactor]indexアクション内のメソッド化

--- app/controllers/festivals/artists_controller.rb
+++ app/controllers/festivals/artists_controller.rb
@@ -1,10 +1,8 @@
 class Festivals::ArtistsController < ApplicationController
   before_action :set_festival
+  before_action :set_festival_days
 
   def index
-    @festival_days = @festival.festival_days.order(:date)
-    @selected_festival_day = @festival_days.find_by(id: params[:festival_day_id]) || @festival_days.first
-
     artists_scope = @festival.artists_for_day(@selected_festival_day)
 
     @q     = artists_scope.ransack(params[:q])
@@ -19,5 +17,10 @@ class Festivals::ArtistsController < ApplicationController
 
   def set_festival
     @festival = Festival.find_by_slug!(params[:festival_id])
+  end
+
+  def set_festival_days
+    @festival_days = @festival.festival_days.order(:date)
+    @selected_festival_day = @festival_days.find_by(id: params[:festival_day_id]) || @festival_days.first
   end
 end
