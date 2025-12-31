d5bd87185e94cc788988496d2d29474404150de2
hinata <hnoguchi132@outlook.jp>
2025-12-31 10:47:45 +0900

    [refactor]uuidロジックの共通化

--- app/models/setlist.rb
+++ app/models/setlist.rb
@@ -1,4 +1,6 @@
 class Setlist < ApplicationRecord
+  include Uuidable
+
   belongs_to :stage_performance
 
   has_many :setlist_songs, dependent: :destroy
@@ -7,8 +9,4 @@ class Setlist < ApplicationRecord
   validates :stage_performance, presence: true, uniqueness: true
 
   accepts_nested_attributes_for :setlist_songs, allow_destroy: true
-
-  def to_param
-    uuid.presence || super
-  end
 end
