70989bd786871806538a7403fa663fbe10394cc2
hinata <hnoguchi132@outlook.jp>
2026-01-02 12:33:46 +0900

    [fix]チェックボックスの付け外し機能の改善

--- spec/requests/packing_list_items_spec.rb
+++ spec/requests/packing_list_items_spec.rb
@@ -5,48 +5,18 @@ RSpec.describe "持ち物リスト項目のリクエスト", type: :request do
   let(:packing_list) { create(:packing_list, user: user) }
   let(:item) { create(:item, user: user) }
 
-  describe "POST /packing_lists/:packing_list_id/packing_list_items" do
-    context "ログイン済みのとき" do
-      before { sign_in user, scope: :user }
-
-      it "項目を追加してリストへリダイレクトする" do
-        expect {
-          post packing_list_packing_list_items_path(packing_list),
-               params: { packing_list_item: { item_id: item.id, note: "メモ", position: 1 } }
-        }.to change { packing_list.packing_list_items.count }.by(1)
-
-        expect(response).to redirect_to(packing_list_path(packing_list))
-        created_item = packing_list.packing_list_items.order(:created_at).last
-        expect(created_item.note).to eq("メモ")
-        expect(created_item.position).to eq(1)
-      end
-    end
-
-    context "未ログインのとき" do
-      it "ログイン画面へリダイレクトし、追加されない" do
-        expect {
-          post packing_list_packing_list_items_path(packing_list),
-               params: { packing_list_item: { item_id: item.id } }
-        }.not_to change(PackingListItem, :count)
-
-        expect(response).to redirect_to(new_user_session_path)
-      end
-    end
-  end
-
   describe "PATCH /packing_lists/:packing_list_id/packing_list_items/:id" do
     let!(:packing_list_item) { create(:packing_list_item, packing_list: packing_list, item: item, note: "旧メモ", position: 0) }
 
-    it "項目の内容を更新してリストへリダイレクトする" do
+    it "チェック状態を更新してリストへリダイレクトする" do
       sign_in user, scope: :user
 
       patch packing_list_packing_list_item_path(packing_list, packing_list_item),
-            params: { packing_list_item: { note: "新しいメモ", position: 2 } }
+            params: { packing_list_item: { checked: true } }
 
       expect(response).to redirect_to(packing_list_path(packing_list))
       packing_list_item.reload
-      expect(packing_list_item.note).to eq("新しいメモ")
-      expect(packing_list_item.position).to eq(2)
+      expect(packing_list_item.checked).to be(true)
     end
 
     it "更新に失敗したら内容は変わらない" do
@@ -68,54 +38,6 @@ RSpec.describe "持ち物リスト項目のリクエスト", type: :request do
 
       expect(response).to have_http_status(:not_found)
       expect(packing_list_item.reload.note).to eq("旧メモ")
-    end
-  end
-
-  describe "PATCH /packing_lists/:packing_list_id/packing_list_items/:id/toggle" do
-    let!(:packing_list_item) { create(:packing_list_item, packing_list: packing_list, item: item, checked: false) }
-
-    it "チェック状態をトグルしてリストへリダイレクトする" do
-      sign_in user, scope: :user
-
-      patch toggle_packing_list_packing_list_item_path(packing_list, packing_list_item)
-
-      expect(response).to redirect_to(packing_list_path(packing_list))
-      expect(packing_list_item.reload.checked).to be(true)
-    end
-
-    it "他人のリストは404を返す" do
-      other_user = create(:user)
-      sign_in other_user, scope: :user
-
-      patch toggle_packing_list_packing_list_item_path(packing_list, packing_list_item)
-
-      expect(response).to have_http_status(:not_found)
-      expect(packing_list_item.reload.checked).to be(false)
-    end
-  end
-
-  describe "DELETE /packing_lists/:packing_list_id/packing_list_items/:id" do
-    let!(:packing_list_item) { create(:packing_list_item, packing_list: packing_list, item: item) }
-
-    it "項目を削除してリストへリダイレクトする" do
-      sign_in user, scope: :user
-
-      expect {
-        delete packing_list_packing_list_item_path(packing_list, packing_list_item)
-      }.to change { packing_list.packing_list_items.count }.by(-1)
-
-      expect(response).to redirect_to(packing_list_path(packing_list))
-    end
-
-    it "他人のリストは404を返す" do
-      other_user = create(:user)
-      sign_in other_user, scope: :user
-
-      expect {
-        delete packing_list_packing_list_item_path(packing_list, packing_list_item)
-      }.not_to change(PackingListItem, :count)
-
-      expect(response).to have_http_status(:not_found)
     end
   end
 end
